3925a8f0e8198fa3a3c059edff71337237b9221c
James Hochadel <james.hochadel@hey.com>
2021-02-15 15:33:45 -0500

    Improve evaluatePosition piece coverage

--- engine/engine_test.go
+++ engine/engine_test.go
@@ -14,10 +14,16 @@ func TestEvaluatePosition(t *testing.T) {
 		Expected float64
 	}{
 		{ "starting position", "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 0.0 },
-		{ "starting position", "rnbqkbnr/ppppppp1/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 1.0 },
+		{ "black missing pawn", "rnbqkbnr/ppppppp1/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 1.0 },
+		{ "white missing pawn", "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPP1/RNBQKBNR w KQkq - 0 1", -1.0 },
+		{ "black no pawns", "rnbqkbnr/8/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 8.0 },
+		{ "black missing bishop", "rn1qkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 3.0 },
+		{ "black missing knight", "r1bqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 3.0 },
+		{ "black missing rook", "1nbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 6.0 },
+		{ "black missing queen", "rnb1kbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 9.0 },
 	}
 	for _, tc := range cases {
-		t.Run(fmt.Sprintf("%v + %v", tc.FEN, tc.Expected), func(t *testing.T) {
+		t.Run(fmt.Sprintf("%v: %v + %v", tc.Name, tc.FEN, tc.Expected), func(t *testing.T) {
 			updateFunc, err := chess.FEN(tc.FEN)
 			if err != nil {
 				t.Fatal("applying FEN to game", err)
