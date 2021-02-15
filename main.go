93d9b833624e370ddaa888c5184c8ecd2a6db4a4
James Hochadel <james.hochadel@hey.com>
2021-02-15 13:19:31 -0500

    Remove unused call to StockFish via UCI

--- main.go
+++ main.go
@@ -5,22 +5,11 @@ import (
 	"log"
 
 	"github.com/notnil/chess"
-	"github.com/notnil/chess/uci"
-	
+
 	"github.com/jameshochadel/chess-bot/engine"
 )
 
 func main() {
-	opponent, err := uci.New("stockfish")
-	if err != nil {
-		panic(err)
-	}
-	defer opponent.Close()
-
-	if err := opponent.Run(uci.CmdUCI, uci.CmdIsReady, uci.CmdUCINewGame); err != nil {
-		panic(err)
-	}
-
 	game := chess.NewGame()
 
 	for game.Outcome() == chess.NoOutcome {
@@ -28,7 +17,7 @@ func main() {
 		// would need to return some results eagerly though?
 		// maybe benchmark to get a sense of how far it can get
 		bestMove := engine.BestMove(game.Position(), game.Position().Turn() == chess.White)
-		if err := game.Move(&bestMove); err != nil {
+		if err := game.Move(bestMove); err != nil {
 			wrapped := fmt.Errorf("making move %v: %w", bestMove, err)
 			log.Fatalf("game state %v, err %v", game.String(), wrapped)
 		}
